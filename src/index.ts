f080c219c180e7059b47dd55219b02917a279e2c
BumTiger <jbpark03@gmail.com>
2025-09-22 15:00:39 +0900

    user interface for the get request response data

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 import axios from "axios";
 
-/*
-{
-  id: 1,
-  name: 'Leanne Graham',
-  username: 'Bret',
-  email: 'Sincere@april.biz',
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
   address: {
-    street: 'Kulas Light',
-    suite: 'Apt. 556',
-    city: 'Gwenborough',
-    zipcode: '92998-3874',
-    geo: { lat: '-37.3159', lng: '81.1496' }
-  },
-  phone: '1-770-736-8031 x56442',
-  website: 'hildegard.org',
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: { lat: string; lng: string };
+  };
+  phone: string;
+  website: string;
   company: {
-    name: 'Romaguera-Crona',
-    catchPhrase: 'Multi-layered client-server neural-net',
-    bs: 'harness real-time e-markets'
-  }
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
-*/
+
 axios
   .get<boolean>("https://jsonplaceholder.typicode.com/users/1")
   .then((res) => {
-    const {data} = res;
+    const { data } = res;
     console.log(res.data);
   })
   .catch((reason) => {
