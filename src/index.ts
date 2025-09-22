38d1bb52e96f2bc68bad1a3e7eb3d057157a4734
BumTiger <jbpark03@gmail.com>
2025-09-22 11:50:28 +0900

    handle get request response - then, catch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import axios from "axios";
 
-axios.get("https://jsonplaceholder.typicode.com/users/1")
+axios
+  .get("https://jsonplaceholder.typicode.com/users/1")
+  .then((res) => {
+    console.log("get 반응 도착");
+  })
+  .catch((reason) => {
+    console.error("get 오류", reason);
+  });
