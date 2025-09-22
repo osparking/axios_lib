6fe8267ed67a304c574379ce0915a36c8ed17135
BumTiger <jbpark03@gmail.com>
2025-09-22 12:00:08 +0900

    log get response data parsed

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 axios
   .get("https://jsonplaceholder.typicode.com/users/1")
   .then((res) => {
-    console.log("get 반응 도착");
+    console.log(res.data);
   })
   .catch((reason) => {
     console.error("get 오류", reason);
